bc7721b7c1bc767a519b4d160c62194a9e2163a0
Anton Ödman <anton.odman@gmail.com>
2023-03-02 16:53:06 +0100

    chore: first init

---
+++ functions/src/func/https.ts
@@ -0,0 +1,10 @@
+import * as functions from "firebase-functions";
+
+export const helloWorld = functions
+    .region("europe-west1")
+    .https.onRequest(
+        (request: functions.Request, response: functions.Response) => {
+            functions.logger.info("Hello logs!", { structuredData: true });
+            response.send("Hello from Firebase!");
+        }
+    );
