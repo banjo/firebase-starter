ef1e90ba776f54b020dae1ac56c5b67217f4037a
Anton Ödman <anton.odman@gmail.com>
2023-03-04 10:56:52 +0100

    chore: add eslint and formatting

--- functions/src/func/https.ts
+++ functions/src/func/https.ts
@@ -2,9 +2,7 @@ import * as functions from "firebase-functions";
 
 export const helloWorld = functions
     .region("europe-west1")
-    .https.onRequest(
-        (request: functions.Request, response: functions.Response) => {
-            functions.logger.info("Hello logs!", { structuredData: true });
-            response.send("Hello from Firebase!");
-        }
-    );
+    .https.onRequest((request: functions.Request, response: functions.Response) => {
+        functions.logger.info("Hello logs!", { structuredData: true });
+        response.send("Hello from Firebase!");
+    });
